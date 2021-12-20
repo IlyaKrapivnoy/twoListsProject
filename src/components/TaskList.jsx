2c01253d7931b0f3a8cd60638a141c10125ff37b
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-20 18:46:37 +0200

    styles for list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -4,14 +4,26 @@ import {
     ListItem,
     ListItemSecondaryAction,
     ListItemText,
+    makeStyles,
+    Typography,
 } from '@material-ui/core';
 import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+    list: {
+        padding: '30px 0',
+    },
+}));
 
 const isChecked = () => {};
 
 const TaskList = () => {
+    const classes = useStyles();
     return (
-        <div>
+        <div className={classes.list}>
+            <Typography variant='h5' gutterBottom>
+                Tasks for Today (num)
+            </Typography>
             <List component='nav' aria-label='main mailbox folders'>
                 <ListItem divider>
                     <ListItemText primary='test task' />
