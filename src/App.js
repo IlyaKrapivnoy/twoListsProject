fd3d0097365af15f7994ca3d57c90070188ac9fd
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-20 18:38:01 +0200

    add wrapper and change TaskList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import { CssBaseline } from '@material-ui/core';
 import Header from './components/Header';
-import TaskList from './components/TaskList';
 import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+import Wrapper from './components/Wrapper';
 
 function App() {
     return (
         <div className='App'>
             <CssBaseline />
             <Header />
-            <TaskForm />
-            <TaskList />
+            <Wrapper>
+                <TaskForm />
+                <TaskList />
+            </Wrapper>
         </div>
     );
 }
