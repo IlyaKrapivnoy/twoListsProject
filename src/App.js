0f451e3d3e6f0e5957d01106a20d39a36a88c238
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-20 18:10:25 +0200

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
 
 function App() {
-    return <div className='App'>Hello World</div>;
+    return (
+        <div className='App'>
+            <CssBaseline />
+            <Header />
+        </div>
+    );
 }
 
 export default App;
