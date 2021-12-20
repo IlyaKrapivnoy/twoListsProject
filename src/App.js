840cf0ab3af0e26ffb06a29c9b5d050bd04ee0ef
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-20 18:30:49 +0200

    add form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { CssBaseline } from '@material-ui/core';
 import Header from './components/Header';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
 
 function App() {
     return (
         <div className='App'>
             <CssBaseline />
             <Header />
+            <TaskForm />
+            <TaskList />
         </div>
     );
 }
