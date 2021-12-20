b5275bd96d8919638ddeeede0b25acdc423143b8
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-20 18:43:27 +0200

    add List

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,27 @@
+import {
+    Checkbox,
+    List,
+    ListItem,
+    ListItemSecondaryAction,
+    ListItemText,
+} from '@material-ui/core';
 import React from 'react';
 
+const isChecked = () => {};
+
 const TaskList = () => {
-    return <div>hi</div>;
+    return (
+        <div>
+            <List component='nav' aria-label='main mailbox folders'>
+                <ListItem divider>
+                    <ListItemText primary='test task' />
+                    <ListItemSecondaryAction>
+                        <Checkbox onChange={isChecked} />
+                    </ListItemSecondaryAction>
+                </ListItem>
+            </List>
+        </div>
+    );
 };
 
 export default TaskList;
